096d479d448c2ee9819c956a2b34f5a9b6cde763
RiteshLuitel60 <69888822+RiteshLuitel60@users.noreply.github.com>
2022-04-08 12:10:21 +0545

    Update Guess_game_forAi.py

--- GuessGame/Guess_game_forAi.py
+++ GuessGame/Guess_game_forAi.py
@@ -16,7 +16,7 @@ def computerGuess():
     # random number in your mind that you want computer to guess
     secret_number = int(input("What you want computer to guess: "))
     
-    # guess_chance is used to give ai the number of tries
+    # guess_chance is used to give computer the number of tries 
     guess_chance = 0
     
     computer_guess = int()
@@ -30,20 +30,21 @@ def computerGuess():
         # useless line of code line 31 and 32    
         # elif secret_number > high:
         #     print("Error! your number is greater than the maximum value. ")    
-    
         
+    
+        # Below line of code sets new value of "low" variable to narrow domn the numbers to guess the correct one.
         if computer_guess < secret_number and guess_chance < 10  and computer_guess != secret_number:
         
             print(f"Ai tried { computer_guess} ")
             low = computer_guess + 1        
             
 
-        
+        # Below line of code sets new value of "high" variable to narrow domn the numbers to guess the correct one.
         elif computer_guess > secret_number and guess_chance < 10 and computer_guess != secret_number :
             print(f"AI TRIED { computer_guess} ")
             high = computer_guess - 1
             
-        
+        # line 47 prints sorry message if computer couldn't guess the secret number wihtin 10 tries.
         elif guess_chance == 10:
             print("computer couldn't guess your number. 😢")
     
